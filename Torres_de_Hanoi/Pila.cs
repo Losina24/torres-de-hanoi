8fc34356da54fc9504f16adb5832f3e829a90e51
Losina <losinagfx@gmail.com>
2021-03-21 15:31:34 +0100

    Clase Disco implementada

---
+++ Torres_de_Hanoi/Pila.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+    class Pila
+    {
+        /* Atributos */
+        public int Size { get; set; }
+        public int Top { get; set; }
+        public List<Disco> Elementos { get; set; }
+
+        /* Métodos */
+        public Pila()
+        {
+            this.Size = 0;
+            this.Top = 0;
+            this.Elementos = new List<Disco>();
+        }
+
+        public void push(Disco d)
+        {
+            Elementos.Add(d);
+            Top = d.Valor;
+            Size = Size++;
+        }
+
+        public Disco pop()
+        {
+            Size = Size--;
+            Disco discoEliminado = Elementos[Size];
+            Elementos.RemoveAt(Size);
+            Top = Elementos[Size--].Valor;
+            return discoEliminado;
+        }                
+
+        public bool isEmpty()
+        {
+            if (Size > 0)
+            {
+                return true;
+            } 
+            else 
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Torres_de_Hanoi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Disco miDisco = new Disco(1);
+            Console.WriteLine("El valor del disco es: " + miDisco.Valor);
+
+            Disco disco2 = new Disco(2);
+            Disco disco3 = new Disco(3);
+            Pila miPila = new Pila();
+            miPila.push(disco3);
+            Console.WriteLine("Disco añadido a la pila, el tamaño de la pila es: " + miPila.Size);
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
